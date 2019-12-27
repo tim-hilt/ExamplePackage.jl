e70f82e5ef7317dadd02c327dfa4d87ed6384192
Tim Hilt <timhilt@live.de>
2019-12-27 09:39:31 +0100

    Added gh-pages-branch

--- src/ExamplePackage.jl
+++
@@ -1,9 +0,0 @@
-module ExamplePackage
-
-greet() = print("Hello World!")
-
-include("extra_file.jl")
-
-export my_f  # If not exported can only be used with a prefixed ExamplePackage.my_f()
-
-end # module
--- test/runtests.jl
+++
@@ -1,8 +0,0 @@
-using ExamplePackage
-using Test
-
-@testset "ExamplePackage.jl" begin
-    # 2x + 3y
-    @test my_f(2, 1) == 7
-    @test my_f(2, 3) == 13
-end
